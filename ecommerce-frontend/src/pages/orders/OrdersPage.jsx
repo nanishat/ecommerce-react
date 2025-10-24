3060a409b96f634f3f16d2af191695f2c224b5bc
nanishat <spy.spid3y@gmail.com>
2025-10-25 00:13:24 +0600

    seprated into smaller components

---
+++ ecommerce-frontend/src/pages/orders/OrdersPage.jsx
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+import Header from '../../components/Header';
+import OrdersGrid from './OrdersGrid';
+
+import './OrdersPage.css';
+
+function OrdersPage({ cart }) {
+
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/orders?expand=products')
+      .then((response) => {
+        setOrders(response.data);
+      })
+  }, []);
+
+  return (
+    <>
+      <link rel="icon" type="image" to="/orders-favicon.png" />
+
+      <title>Orders</title>
+
+      <Header cart={cart} />
+
+      <div className="orders-page">
+        <div className="page-title">Your Orders</div>
+        <OrdersGrid orders={orders} />
+      </div>
+    </>
+  );
+}
+
+export default OrdersPage;
