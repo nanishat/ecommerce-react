6c05afc965cace720dd686a2575375ce99f2a0ad
nanishat <spy.spid3y@gmail.com>
2025-11-02 23:52:26 +0600

    fetch data from backend for tracking page and replace hard coded data

--- ecommerce-frontend/src/pages/TrackingPage.jsx
+++ ecommerce-frontend/src/pages/TrackingPage.jsx
@@ -1,12 +1,30 @@
-import { Link } from 'react-router';
-import { useParams } from 'react-router';
+import axios from 'axios';
+import { Link, useParams } from 'react-router';
+import { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import './TrackingPage.css';
+import dayjs from 'dayjs';
 
 function TrackingPage({ cart }) {
 
-  const params = useParams();
-  console.log(params);
+  const { orderId, productId } = useParams();
+  const [order, setOrder] = useState(null);
+
+  useEffect(() => {
+    const fetchTrackingPage = async () => {
+      const respone = await axios.get(`/api/orders/${orderId}?expand=products`);
+      setOrder(respone.data);
+    }
+    fetchTrackingPage();
+  }, [orderId]);
+
+  if (!order) {
+    return null;
+  }
+
+  const orderProduct = order.products.find((orderProduct) => {
+    return orderProduct.productId === productId;
+  })
 
   return (
     <>
@@ -23,18 +41,18 @@ function TrackingPage({ cart }) {
           </Link>
 
           <div className="delivery-date">
-            Arriving on Monday, June 13
+            Arriving on {dayjs(orderProduct.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
           </div>
 
           <div className="product-info">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
+            {orderProduct.product.name}
           </div>
 
           <div className="product-info">
-            Quantity: 1
+            Quantity: {orderProduct.quantity}
           </div>
 
-          <img className="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+          <img className="product-image" src={orderProduct.product.image} />
 
           <div className="progress-labels-container">
             <div className="progress-label">
