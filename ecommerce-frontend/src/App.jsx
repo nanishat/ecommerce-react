726db8c96c2e848c9c74e2185ada5f6ecf3b7e78
nanishat <spy.spid3y@gmail.com>
2025-11-06 00:24:02 +0600

    coded data mutation for delivery options

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -23,8 +23,8 @@ function App() {
   return (
     <Routes>
       <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
-      <Route path='checkout' element={<CheckoutPage cart={cart} />} />
-      <Route path='orders' element={<OrdersPage cart={cart} />} />
+      <Route path='checkout' element={<CheckoutPage cart={cart} loadCart={loadCart} />} />
+      <Route path='orders' element={<OrdersPage cart={cart} loadCart={loadCart} />} />
       <Route path='tracking/:orderId/:productId' element={<TrackingPage cart={cart} />} />
       <Route path='*' element={<NotFoundPage cart={cart} />} />
     </Routes>
--- ecommerce-frontend/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-frontend/src/pages/checkout/OrderSummary.jsx
@@ -2,7 +2,7 @@ import DeliveryDate from "./DeliveryDate";
 import CartItemDetails from "./CartItemDetails";
 import DeliveryOptions from "./DeliveryOptions";
 
-function OrderSummary({ cart, deliveryOptions }) {
+function OrderSummary({ cart, deliveryOptions, loadCart }) {
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 && cart.map((cartItem) => {
@@ -18,7 +18,7 @@ function OrderSummary({ cart, deliveryOptions }) {
             <div className="cart-item-details-grid">
               <CartItemDetails cartItem={cartItem} />
 
-              <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} />
+              <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} loadCart={loadCart} />
             </div>
           </div>
         );
