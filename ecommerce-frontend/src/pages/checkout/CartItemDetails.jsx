f23cea00a0d828459c1578e200c01bef95e3738a
nanishat <spy.spid3y@gmail.com>
2025-11-07 20:31:33 +0600

    added toggle option in the update quantity option

--- ecommerce-frontend/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-frontend/src/pages/checkout/CartItemDetails.jsx
@@ -1,7 +1,18 @@
 import axios from 'axios';
+import { useState } from 'react';
 import formatCurrency from "../../utils/formatCurrency";
 
 function CartItemDetails({ cartItem, loadCart }) {
+
+  const [isUpdatingQuantity, setIsUpdatingQuantity] = useState(false);
+
+  const updateQuantity = () => {
+    if (isUpdatingQuantity) {
+      setIsUpdatingQuantity(false);
+    } else {
+      setIsUpdatingQuantity(true);
+    }
+  }
 
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
@@ -22,9 +33,13 @@ function CartItemDetails({ cartItem, loadCart }) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity: <span className="quantity-label">{cartItem.quantity}</span>
+            Quantity:
+            {isUpdatingQuantity
+              ? <input type="text" className='update-quantity' />
+              : <span className="quantity-label">{cartItem.quantity}</span>
+            }
           </span>
-          <span className="update-quantity-link link-primary">
+          <span className="update-quantity-link link-primary" onClick={updateQuantity}>
             Update
           </span>
           <span className="delete-quantity-link link-primary" onClick={deleteCartItem}>
